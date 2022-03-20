adc162965d028744bb3e724b0b02dd74c09d3282
Wiltter Freitas <wiltterfreitasw@gmail.com>
2022-03-20 20:13:47 -0300

    context

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,25 +3,17 @@ import { Link } from 'react-router-dom';
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
 const LoginForm = () => {
 
 	const username = useForm();
 	const password = useForm();
+	const {userLogin} = React.useContext(UserContext);
 
-  function handleSubmit(event){
+  async function handleSubmit(event){
 		event.preventDefault();
 		if (username.validate() && password.validate()) {
-			fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token",{
-				method: "POST",
-				headers:{
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify(),
-			}).then((response) => {console.log(response)
-				return response.json();
-			}).then((json) => {
-				console.log(json);
-			})
+			userLogin(username.value, password.value)
 		}
   }
 
